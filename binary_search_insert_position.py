a2a9f70155d5329636e08ae9c82094b626f37e32
andikapratama25 <133621392+andikapratama25@users.noreply.github.com>
2023-05-22 13:23:57 +0700

    Add files via upload

---
+++ binary_search_insert_position.py
@@ -0,0 +1,21 @@
+def binary_search_insert_position(data, target):
+    left = 0
+    right = len(data) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+        if data[mid] == target:
+            return mid
+        elif data[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return left
+
+data = [2, 4, 6, 8, 10, 12, 14]
+target = 7
+
+insert_position = binary_search_insert_position(data, target)
+
+print(f"Elemen {target} dapat disisipkan pada indeks {insert_position} untuk menjaga daftar tetap terurut.")
